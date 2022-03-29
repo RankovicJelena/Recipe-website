7ffdb40819908c3de9766b954333f85a554efc94
Jelena <jelenarankovic0@gmail.com>
2022-03-29 12:07:54 +0200

    uploading new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'aaf9f098-9533-4ed9-be09-b143fbf3097a';
+export const MODAL_CLOSE_SEC = 2.5;
